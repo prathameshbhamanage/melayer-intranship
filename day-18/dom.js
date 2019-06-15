ec1afb72d9cbb9fe834c7c059eae2a4207262dc0
prathamshbhamanage07@gmail.com <prathameshbhamanage07@gmail.com>
2019-06-15 13:03:19 +0530

    get element by id

--- day-18/dom.js
+++ day-18/dom.js
@@ -1,6 +1,9 @@
-console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.head);
-console.log(document.title);
-console.log(document.h1);
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.head);
+// console.log(document.title);
+// console.log(document.h1);
+
+var data=document.getElementById="headr-title";
+console.log(data);
