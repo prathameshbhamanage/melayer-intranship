5ca5222a1caa725b257d63c75b8860e015da1f79
prathamshbhamanage07@gmail.com <prathameshbhamanage07@gmail.com>
2019-06-15 14:48:37 +0530

    get element by tagname

--- day-18/dom.js
+++ day-18/dom.js
@@ -1,12 +1,14 @@
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.head);
-// console.log(document.title);
-// console.log(document.h1);
+// // console.dir(document);
+// // console.log(document.domain);
+// // console.log(document.URL);
+// // console.log(document.head);
+// // console.log(document.title);
+// // console.log(document.h1);
 
-// var data=document.getElementById="headr-title";
-// console.log(data);
- var items =document.getElementsByClassName="list-group";
- console.log(items[1]);
- items[1].style.backgraundColor ='yellow' ;
+// // var data=document.getElementById="headr-title";
+// // console.log(data);
+//  var items =document.getElementsByClassName="list-group";
+//  console.log(items[1]);
+//  items[1].style.backgraundColor ='yellow' ;
+var data=document.getElementsByTagName="li";
+console.log(data);
