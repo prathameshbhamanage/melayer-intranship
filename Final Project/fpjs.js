1dee7a883974fe5ae6ce997a8630f677d70e1731
prathamshbhamanage07@gmail.com <prathameshbhamanage07@gmail.com>
2019-06-22 16:28:12 +0530

    final project

---
+++ Final Project/fpjs.js
@@ -0,0 +1,111 @@
+function todslide1(){
+    $("#hall1").slideToggle("slow");
+    // $("#i1").css("transform","rotate(90deg)");
+    $("#ha1").toggle();
+    $("#img1").toggle();
+
+
+    $("#ac").toggle();
+    $("#fan1").toggle();
+    $("#fan2").toggle();
+    $("#bulb").toggle();
+
+}
+
+
+
+function hlight1(){
+
+    $("#bulb").toggle();
+    $("#hlight").toggle();
+    
+}
+
+function hfan1(){
+    $("#fan1").toggle();
+
+    $("#hfan1").toggle();
+
+    $("#fan2").toggle();
+    $("#hfan2").toggle();
+
+}
+
+function hac(){
+    $("#ac").toggle();
+    $("#hac").toggle();
+
+}
+
+//bedroom
+
+function bbulb(){
+
+    $("#bbulb").toggle();
+    $("#blight").toggle();
+    
+}
+
+function bac(){
+
+    $("#bac").toggle();
+    $("#bac1").toggle();
+    
+}
+
+function bfan(){
+
+    $("#bfan").toggle();
+    $("#bfan1").toggle();
+    
+}
+
+function todslide2(){
+    $("#hall2").slideToggle("slow");
+    $("#ha2").toggle();
+    $("#img1").toggle();
+
+    $("#bfan").toggle();
+    $("#bac").toggle();
+    $("#bbulb").toggle();
+}
+
+//kitchen
+function todslide3(){
+    $("#hall3").slideToggle("slow");
+    $("#ha3").toggle();
+    $("#img1").toggle();
+    $("#kbulb").toggle();
+    $("#kfan").toggle();
+}
+
+function kfan(){
+
+    $("#kfan").toggle();
+    $("#kfan1").toggle();
+    
+}
+
+function kbulb(){
+
+    $("#kbulb").toggle();
+    $("#klight").toggle();
+    
+}
+
+//bathroom
+
+
+function todslide4(){
+    $("#hall4").slideToggle("slow");
+    $("#ha4").toggle();
+    $("#img1").toggle();
+    $("#bebulb").toggle();
+}
+
+function bebulb(){
+
+    $("#bebulb").toggle();
+    $("#belight").toggle();
+    
+}
